9b6739250a98797a1ec47e2d910b8ab1d548a858
key-er <nasaquba@gmail.com>
2018-08-30 01:34:03 -0700

    added file listing component

---
+++ client/src/components/BrowseFiles.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Table } from 'semantic-ui-react'
+
+
+const BrowseFileItems = (props) => (
+
+    <Table.Row>
+      <Table.Cell>{props.item}</Table.Cell>
+    </Table.Row>
+  )
+
+
+const BrowseFiles = (props) => {
+
+  const listItems = props.folders.map( (item, idx) => {
+    return <BrowseFileItems key={idx} item={item}/>
+  })
+
+
+  return (
+    <Table celled selectable>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>{props.type}</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {listItems}
+      </Table.Body>
+    </Table>
+    )
+}
+
+export default BrowseFiles
+
+
