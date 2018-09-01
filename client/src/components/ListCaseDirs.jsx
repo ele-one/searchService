b1b35e53b246bb806d25f90eff76bb51709d9e07
Kausar Khizra <khizra6@gmail.com>
2018-08-31 21:16:42 -0700

    moved first ajax call to componentDidMount method

--- client/src/components/ListCaseDirs.jsx
+++ client/src/components/ListCaseDirs.jsx
@@ -17,11 +17,20 @@ class ListCaseDirs extends React.Component {
 
     this.fetchCaseDirs = this.fetchCaseDirs.bind(this);
     this.handleClick = this.handleClick.bind(this);
-    this.fetchCaseDirs(this.props.selectedLogType);
 
     this.state = {
       caseDirs: [],
       clickedDir:null
+    }
+  }
+
+  componentDidMount() {
+    this.fetchCaseDirs(this.props.selectedLogType);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.selectedLogType !== prevProps.selectedLogType) {
+      this.fetchCaseDirs(this.props.selectedLogType);
     }
   }
 
@@ -51,13 +60,6 @@ class ListCaseDirs extends React.Component {
     this.props.handleCaseDirSelection(clickedDir);
   }
 
-
-
-  componentDidUpdate(prevProps) {
-    if (this.props.selectedLogType !== prevProps.selectedLogType) {
-      this.fetchCaseDirs(this.props.selectedLogType);
-    }
-  }
 
   render() {
 
